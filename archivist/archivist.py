a8aea70994ea6093c2a122fc8399337c3ec7ed05
metrizulu <stephenson.shane.a@gmail.com>
2020-12-18 01:43:55 -0500

    got reworked commands to work as expected

--- archivist/archivist.py
+++ archivist/archivist.py
@@ -11,41 +11,38 @@ from datetime import datetime, timedelta
 class Archivist(commands.Cog):
     def __init__(self, paywalled_sites):
         self.paywalled_sites = paywalled_sites
-
-    @commands.command(name = 'paywall')
-    async def archive_link(self, ctx, link: str):
-        # Manually link to archive.is
-        # Format: `!paywall URL` will link to archive.is/URL
-        if url_validator(link):
-            await ctx.send(f"https://www.archive.is/{link}")
-        else:
-            await ctx.send(f"**{link}** is an invalid url.")
     
-    @commands.command()
-    async def add(self,ctx, *new_paywalls):
-        # Add new domains to list of paywalled domains
-        # Format: `!add DOMAIN_1 DOMAIN_2 ... DOMAIN_n` will add DOMAIN_1 thru DOMAIN_n to list
-        #     of paywalled sites and respond with a confirmation message.
-        self.paywalled_sites = set(self.paywalled_sites).union(set(new_paywalls))
-        async with open('paywalled', 'w') as file:
-            sites = "\n".join(self.paywalled_sites)
-            await file.write(sites)
-            await ctx.send('**Added the following domains:**' + "\n" + cprint(new_paywalls))
-
-    @commands.command()
-    async def delete(self, ctx, *remove_sites):
-        # Delete domains to list of paywalled domains
-        # Format: `!add DOMAIN_1 DOMAIN_2 ... DOMAIN_n` will add DOMAIN_1 thru DOMAIN_n to list
-        #     of paywalled sites and respond with a confirmation message.
-        self.paywalled_sites = set(self.paywalled_sites).difference(set(remove_sites))
-        async with open('paywalled', 'w') as file:
-            sites = "\n".join(paywalled_sites)
-            await file.write(sites)
-            await ctx.send('**Deleted the following domains:**' + "\n" + cprint(remove_sites))
-    
-    @commands.command(name = 'list paywalls')
-    async def list_paywalls(self, ctx, arg:str):
-        await ctx.send("**Paywalled sites:**" + "\n" + cprint(sorted(paywalled_sites)))
+    @commands.command(name = 'paywalls')
+    async def paywalls(self, ctx, *args):
+        if args[0] == 'add':
+            # Add new domains to list of paywalled domains
+            # Format: `!paywalls add DOMAIN_1 DOMAIN_2 ... DOMAIN_n` will add DOMAIN_1 thru DOMAIN_n to list
+            #     of paywalled sites and respond with a confirmation message.
+            self.paywalled_sites = list(set(self.paywalled_sites).union(set(args[1:])))
+            with open('paywalled', 'w') as file:
+                sites = "\n".join(self.paywalled_sites)
+                file.write(sites)
+                await ctx.send('**Added the following domains:**' + "\n" + cprint(args[1:]))
+        elif args[0] == 'list':
+            # List all paywalled sites currently tracking
+            # Format: `!paywalls list` will list all paywalled sites
+            await ctx.send("**Paywalled sites:**" + "\n" + cprint(sorted(self.paywalled_sites)))
+        elif args[0] == 'delete':
+            # Delete domains to list of paywalled domains
+            # Format: `!paywalls delete DOMAIN_1 DOMAIN_2 ... DOMAIN_n` will add DOMAIN_1 thru DOMAIN_n to list
+            #     of paywalled sites and respond with a confirmation message.
+            self.paywalled_sites = list(set(self.paywalled_sites).difference(set(args[1:])))
+            with open('paywalled', 'w') as file:
+                sites = "\n".join(self.paywalled_sites)
+                file.write(sites)
+                await ctx.send('**Deleted the following domains:**' + "\n" + cprint(args[1:]))
+        elif args[0] == 'link':
+            # Manually link to archive.is
+            # Format: `!paywalls link URL` will link to archive.is/URL
+            if url_validator([args[1]]):
+                await ctx.send(f"https://www.archive.is/{args[1]}")
+            else:
+                await ctx.send(f"**{args[1]}** is an invalid url.")
 
     @commands.Cog.listener()
     async def on_message(self, message):
