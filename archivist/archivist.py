903b01dfeca4d0dfd18d2dc36111adac69c83929
metriczulu <stephenson.shane.a@gmail.com>
2022-10-28 23:42:06 -0400

    small formatting change

--- archivist/archivist.py
+++ archivist/archivist.py
@@ -8,17 +8,18 @@ from collector import Collector
 
 
 if __name__ == "__main__":
-    """local resource loads"""
+    # local resource loads
     # load paywalled sites
     paywalled_sites = load_paywalls()
     # load bot token
     token = load_token()
 
-    """bot instantiation"""
+    # bot instantiation
     # creates discord bot object (with member intents enabled to grab members)
     intents = discord.Intents.default()
     intents.members = True
     bot = commands.Bot(intents=intents, command_prefix="!", case_insensitive=True)
+
     # add command cogs to bot
     bot.add_cog(Librarian(paywalled_sites))  # archive is commands and listener
     bot.add_cog(RateLimiter())  # gripe at sal and aj when they fight
@@ -26,5 +27,5 @@ if __name__ == "__main__":
     bot.add_cog(Creeper(bot))  # listen to say weird things
     bot.add_cog(Wordle(bot))  # calculate wordle stats
     bot.add_cog(Collector(bot))  # aggregate yee data
-    # run the bot
-    bot.run(token)
+    bot.run(token) # run the bot
+
